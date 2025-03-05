5d34b70a63c9ee13cf2f868127e7cea846cf8097
farouq-cy <149035576+farouq-cy@users.noreply.github.com>
2025-03-06 00:27:40 +0200

    farouq

--- BACK-END/project/saler/urls.py
+++ BACK-END/project/saler/urls.py
@@ -1,14 +1,21 @@
-from django.db import models
-
-# Create your models here.
-
-class Product(models.Model):
-    name = models.CharField(max_length=255, verbose_name="اسم المنتج", blank=True, null=True, default="منتج بدون اسم")
-    description = models.TextField(verbose_name="الوصف", blank=True, null=True)
-    color = models.CharField(max_length=50, verbose_name="اللون", blank=True, null=True)
-    category = models.CharField(max_length=100,null=True, blank=True, verbose_name="التصنيف")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="السعر", default=0.00)
-    image = models.ImageField(upload_to='products/', default='default.jpg', verbose_name="صورة المنتج", blank=True, null=True)
-
-    def __str__(self):
-        return self.name
+from django.urls import path
+from . import views
+from .views import *
+urlpatterns = [
+    path('index/' ,views.index, name='index'),
+    path('about/' ,views.about, name='about'),
+    path('login/' ,views.user_login, name='login'),
+    path('register/' ,views.register, name='register'),
+    path('contact/' ,views.contact_view, name='contact'),
+    path('allproducts/' ,views.allproducts, name='allproducts'),
+    path('wishlist/' ,views.wishlist, name='wishlist'),
+    path('toggle-like/<int:product_id>/', toggle_like, name='toggle_like'),
+    path('product/<int:pk>/', views.product, name='product'),
+    path('cart/', views.cart, name='cart'),
+    path('add-to-cart/', add_to_cart, name='add_to_cart'),
+    path("remove_from_cart/<str:product_id>/", remove_from_cart, name="remove_from_cart"),
+    path("update-cart/", update_cart, name="update_cart"),
+    path("logout/", views.logout_view, name="logout"),
+    path("account/", views.account, name="account"),
+    path("saler/", views.saler,name="saler.urls"),
+]
