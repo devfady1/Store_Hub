ab4fbb0c95626823293f225e3dccc3c263ffe98d
farouq-cy <149035576+farouq-cy@users.noreply.github.com>
2025-03-06 00:14:41 +0200

    farouq

--- BACK-END/project/saler/views.py
+++ BACK-END/project/saler/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect  
+from .forms import ProductForm 
 # Create your views here.
+
+def add_product(request):
+    if request.method == "POST":  
+        form = ProductForm(request.POST, request.FILES)  
+        if form.is_valid():  
+            form.save()  
+            return redirect("success") 
+    else:
+        form = ProductForm() 
+
+    return render(request, "seller/seller.html", {"form": form}) 
+
+def success_view(request):
+    return render(request, "seller/success.html") 
+
+
