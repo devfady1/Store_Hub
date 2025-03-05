feb2d3633dc17a90c96b408c7dcf4a44e437a727
devfady1 <fadya5323@gmail.com>
2025-03-05 22:35:21 +0000

    الله يحرقك يفاروق

--- BACK-END/project/saler/urls.py
+++ BACK-END/project/saler/urls.py
@@ -2,20 +2,5 @@ from django.urls import path
 from . import views
 from .views import *
 urlpatterns = [
-    path('index/' ,views.index, name='index'),
-    path('about/' ,views.about, name='about'),
-    path('login/' ,views.user_login, name='login'),
-    path('register/' ,views.register, name='register'),
-    path('contact/' ,views.contact_view, name='contact'),
-    path('allproducts/' ,views.allproducts, name='allproducts'),
-    path('wishlist/' ,views.wishlist, name='wishlist'),
-    path('toggle-like/<int:product_id>/', toggle_like, name='toggle_like'),
-    path('product/<int:pk>/', views.product, name='product'),
-    path('cart/', views.cart, name='cart'),
-    path('add-to-cart/', add_to_cart, name='add_to_cart'),
-    path("remove_from_cart/<str:product_id>/", remove_from_cart, name="remove_from_cart"),
-    path("update-cart/", update_cart, name="update_cart"),
-    path("logout/", views.logout_view, name="logout"),
-    path("account/", views.account, name="account"),
-    path("saler/", views.saler,name="saler.urls"),
+    path("saler/", views.add_product,name="saler.urls"),
 ]
